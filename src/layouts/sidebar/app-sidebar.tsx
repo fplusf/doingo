e6667a23d994f5f481a29981289534c83dccdf36
Farukh Fuzaylov <fuzailof@gmail.com>
2025-04-23 23:32:53 +0500

    chore: comment out inbox for now

--- src/layouts/sidebar/app-sidebar.tsx
+++ src/layouts/sidebar/app-sidebar.tsx
@@ -15,7 +15,7 @@ import { toast } from '@/shared/hooks/use-toast';
 import { Link, useNavigate } from '@tanstack/react-router';
 import { useStore } from '@tanstack/react-store';
 import { format } from 'date-fns';
-import { Bell, Calendar1Icon, Inbox, LucideFocus } from 'lucide-react';
+import { Bell, Calendar1Icon, LucideFocus } from 'lucide-react';
 import * as React from 'react';
 
 const data = {
@@ -147,7 +147,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarMenuItem>
         </SidebarMenu>
 
-        <SidebarMenu>
+        {/* <SidebarMenu>
           <SidebarMenuItem className="flex flex-col items-center">
             <SidebarMenuButton size="lg" asChild>
               <Link
@@ -162,7 +162,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             </SidebarMenuButton>
             <span className="mt-0.5 truncate text-xs">Inbox</span>
           </SidebarMenuItem>
-        </SidebarMenu>
+        </SidebarMenu> */}
 
         <SidebarMenu>
           <SidebarMenuItem className="flex flex-col items-center">
